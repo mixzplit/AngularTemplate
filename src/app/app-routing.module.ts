3ee6da9ed2eabd6f48e591a34e273a971f5c1063
Acurero David <DavidAcurero@tupperware.com>
2022-02-24 20:23:58 -0300

    Integracion con PDFTRON para mostrar documentos.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,11 +14,11 @@ const routes: Routes = [
       { path: 'cierrePedidos', loadChildren: () => import('./pages/cierre-pedidos/cierre-pedidos.module').then( m => m.CierrePedidosModule), canActivate: [RoleGuard]},
       { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) },
       { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule), canActivate: [RoleGuard] },
-      { path: 'documentos', loadChildren: () => import('./pages/documentos/documentos.module').then(m => m.DocumentosModule) },
       { path: 'offers', loadChildren: () => import('./pages/offers/offers.module').then(m => m.OffersModule), canActivate: [RoleGuard] },
     ],
     canActivate: [AuthGuard]
   },
+  { path: 'documentos', loadChildren: () => import('./pages/documentos/documentos.module').then(m => m.DocumentosModule) },
   { path: 'pageNotFound', loadChildren: () => import('./pages/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule) },
   { path: '**', pathMatch: 'full', redirectTo: '/pageNotFound' }
 ];
